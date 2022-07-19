7a839f43622eac014b25f7b47b67847558ce3655
Wellington R <sousa_wellington@outlook.com>
2022-07-19 00:06:58 -0300

    build⬆️: add Chakra UI and upgrade depences

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react'
 
 const MyApp: React.FC<AppProps> = function ({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  )
 };
 
 export default MyApp;
